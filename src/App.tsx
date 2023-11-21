b95869e761d02ac8eb0f11288581a78c23208b52
Serge044 <serhiima@wix.com>
2023-11-21 17:46:56 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useForm } from "react-hook-form";
+
+type Profile = {
+  firstname: string;
+  lastname: string;
+  age: number;
+};
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Profile>();
+
+  const onSubmit = handleSubmit((data) => {
+    alert(JSON.stringify(data));
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <label htmlFor="firstname">First name</label>
+        <input
+          {...register("firstname", {
+            required: true,
+          })}
+          id="firstname"
+          name="firstname"
+          type="text"
+        />
+        {errors.firstname && <div className="error">Enter your name</div>}
+      </div>
+      <div>
+        <label htmlFor="lastname">Last name</label>
+        <input
+          {...register("lastname", {
+            required: true,
+          })}
+          id="lastname"
+          name="lastname"
+          type="text"
+        />
+        {errors.lastname && <div className="error">Enter your lastname</div>}
+      </div>
+      <div>
+        <label htmlFor="age">Age</label>
+        <input
+          {...register("age", {
+            // set value as number
+            valueAsNumber: true,
+            required: true,
+          })}
+          id="age"
+          name="age"
+          type="number"
+        />
+        {errors.age && <div className="error">Enter your age</div>}
+      </div>
+      <button type="submit">Save</button>
+    </form>
   );
 }
 
